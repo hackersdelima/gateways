e670de16276900aab2ade4a25397afc61ddf1871
Shishir Karki <skarki2@gmail.com>
2023-07-29 18:45:56 +0545

    FEAT: add api response class

--- src/main/java/com/shishir/gateways/commons/ApiResponse.java
+++ src/main/java/com/shishir/gateways/commons/ApiResponse.java
@@ -1,2 +1,27 @@
-package com.shishir.gateways.commons;public class ApiResponse {
+package com.shishir.gateways.commons;
+
+import lombok.Builder;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@ToString
+@Builder
+public class ApiResponse {
+    private int httpStatus;
+    private String message;
+    private List<String> errors;
+
+    public ApiResponse badRequest() {
+        this.httpStatus = HttpStatus.BAD_REQUEST.value();
+        this.message = HttpStatus.BAD_REQUEST.getReasonPhrase();
+        return this;
+    }
+
+    public ApiResponse success() {
+        this.httpStatus = HttpStatus.OK.value();
+        this.message = HttpStatus.OK.getReasonPhrase();
+        return this;
+    }
 }
